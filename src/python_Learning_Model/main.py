91f9de4fc24612f553be5cfab474838622947f6f
Kanishka Mitra <ankhsaki@gmail.com>
2024-11-08 00:23:22 -0500

    setting up parameter class

---
+++ src/python_Learning_Model/main.py
@@ -0,0 +1,8 @@
+import setParameters 
+
+
+def execute_main(x):
+    print("running script: ", x)
+
+
+if __name__ == 
---
+++ src/python_Learning_Model/setParameters.py
@@ -0,0 +1,6 @@
+class paramFixed:
+    def __init__(self):
+        self.r = 3.0
+        self.i = 10.5
+
+params = paramFixed(
